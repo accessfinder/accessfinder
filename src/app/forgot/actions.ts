aa950927692445f04c951449b86a368e63de4b47
Naya Singhania <naya.singhania@gmail.com>
2024-12-24 15:22:02 -0800

    update reset password action

--- src/app/forgot/actions.ts
+++ src/app/forgot/actions.ts
@@ -4,5 +4,8 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function forgot(formData: FormData) {
   const supabase = await createClient();
-  await supabase.auth.resetPasswordForEmail(formData.get("email") as string);
+  const options =  {
+      redirectTo: process.env.SITE_URL,
+  }
+  await supabase.auth.resetPasswordForEmail(formData.get("email") as string, options);
 }
